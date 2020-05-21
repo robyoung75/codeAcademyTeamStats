0436374d143d1abcc8263233db9dd84baafdc202
rob <52644668+robyoung75@users.noreply.github.com>
2020-05-21 13:20:26 -0600

    razorBacks object with array properties _players and _games

---
+++ main.js
@@ -0,0 +1,26 @@
+// codeAcademy teamStats project
+
+const razorBacks = { //object razorBacks
+    _players: [ // _players a property of object razorBacks _players is an array
+        {
+            firstName: 'Brighton', // properties and values of _players, populates the _players array. Each player listed on a new line.
+            lastName: 'Young',
+            age: 13
+        },
+        {
+            firstName: 'Blake',
+            lastName: 'Young',
+            age: 13
+        },
+        {
+            firstName: 'Zach',
+            lastName: 'Burns',
+            age: 13
+        }
+    ],
+    _games: [] // _games a property of object razorBacks _games is an array
+};  
+
+console.log(razorBacks._players) // logs each players first name, last name, and age.
+  
+  
